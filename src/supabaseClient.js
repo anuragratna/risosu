fb1cf6c42aae68758ae01c9841c4439b454e0371
Anurag <anuragratna@gmail.com>
2026-01-19 14:28:19 +0100

    Secure credentials using .env and verify all features

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://pnmqszwishmdwuwronwi.supabase.co'
-const supabaseKey = 'sb_publishable_b3Ur_dFSZMKOIAKkgMzn0A_RLevlh6U'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
